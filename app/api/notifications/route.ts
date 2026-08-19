d1789910b569db94c4dbfa830492949cb8107c9c
Zubair <zubair.haider31@gmail.com>
2026-08-19 19:37:13 +0500

    fixing issue

--- app/api/notifications/route.ts
+++ app/api/notifications/route.ts
@@ -24,7 +24,7 @@ export async function GET(req: NextRequest) {
       createdAt: notes.createdAt,
     })
     .from(notes)
-    .where(gt(notes.createdAt, sinceDate.toISOString() as any));
+    .where(gt(notes.createdAt, sinceDate));
 
   return NextResponse.json({ notes: newNotes });
 }
